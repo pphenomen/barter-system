f73a806820829d76ab4dc13cf771f990df0d2a1f
pphenomen <pphenomen44@gmail.com>
2025-06-01 10:58:48 +0300

    выбор категорий и обновленные миграции

--- ads/models.py
+++ ads/models.py
@@ -8,12 +8,19 @@ class Ad(models.Model):
         ('new', 'Новый'),
         ('used', 'Б/у'),
     ]
+    
+    CATEGORIES = [
+        ('Недвижимость', 'Недвижимость'),
+        ('Транспорт', 'Транспорт'),
+        ('Личные вещи', 'Личные вещи'),
+        ('Электроника', 'Электроника'),
+    ]
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
     title = models.CharField(max_length=255, verbose_name='Заголовок')
     description = models.TextField(verbose_name='Описание')
     image_url = models.URLField(blank=True, null=True, verbose_name='Ссылка на изображение')
-    category = models.CharField(max_length=100, verbose_name='Категория')
+    category = models.CharField(max_length=100, choices=CATEGORIES, verbose_name='Категория')
     condition = models.CharField(max_length=20, choices=CONDITION_CHOICES, verbose_name='Состояние')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
 
